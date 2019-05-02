aaef7ac5ae0a98d3c89524357f593c71167b0c06
Ivan <machuk.ivan@yandex.by>
2019-05-02 19:06:48 +0300

    added promises

--- script.js
+++ script.js
@@ -3,19 +3,39 @@
 let inputByn = document.getElementById('byn'),
     inputUsd = document.getElementById('usd');
 
-inputByn.addEventListener('input', () => {
-    let request = new XMLHttpRequest();
+function getUrl(url) {
+    return new Promise(function (resolve, reject) {
+        let request = new XMLHttpRequest();
 
-    request.open('GET', 'current.json');
-    request.setRequestHeader('Content-type', 'application/json; charset=utf-8');
-    request.send();
+        request.open('GET', url);
+        request.setRequestHeader('Content-type', 'application/json; charset=utf-8');
+        request.send();
 
-    request.addEventListener('readystatechange', function() {
-        if (request.readyState === 4 && request.status == 200) {
-            let data = JSON.parse(request.response);
-            inputUsd.value = (inputByn.value / data.usd).toFixed(2);
-        } else {
-            inputUsd.value = 'Произошла ошибка';
-        }
+        request.addEventListener('readystatechange', function () {
+            if (this.readyState < 4) {
+                inputUsd.value = 'Loading';
+            } else if (this.readyState === 4 && this.status == 200) {
+                resolve(this.response);
+            } else {
+                let error = new Error(this.statusText);
+                error.code = this.status;
+                reject(error);
+            }
+        });
     });
+}
+
+inputByn.addEventListener('input', function () {
+    getUrl('current.json')
+        .then(response => {
+            let data = JSON.parse(response);
+            return data;
+        })
+        .then(data => {
+            inputUsd.value = (inputByn.value / data.usd).toFixed(2);
+        })
+        .catch(error => {
+            console.log(error);
+            inputUsd.value = 'Произошла ошибка';
+        });
 });
